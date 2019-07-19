7a2d7772b8190fa1cdc3dff699fec880c0062ccd
Luciano Mattos <luciano.mattos@radixeng.com.br>
2019-07-19 00:47:39 -0300

    Inicializando projeto de exemplo para meetup sobre teste de unidade

---
+++ Meetup.UnitTestExample.Domain/Model/BaseEntity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Meetup.UnitTestExample.Domain.Model
+{
+    public abstract class BaseEntity : IEquatable<BaseEntity>
+    {
+        public int Id { get; protected set; }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as BaseEntity);
+        }
+
+        public bool Equals(BaseEntity other)
+        {
+            return other != null &&
+                   Id.Equals(other.Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+
+        public static bool operator ==(BaseEntity entity1, BaseEntity entity2)
+        {
+            return EqualityComparer<BaseEntity>.Default.Equals(entity1, entity2);
+        }
+
+        public static bool operator !=(BaseEntity entity1, BaseEntity entity2)
+        {
+            return !(entity1 == entity2);
+        }
+    }
+}
---
+++ Meetup.UnitTestExample.Domain/Repository/IBaseRepository.cs
@@ -0,0 +1,22 @@
+﻿using Meetup.UnitTestExample.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Meetup.UnitTestExample.Domain.Repository
+{
+    public interface IBaseRepository<TEntity> :
+        IDisposable where TEntity : BaseEntity
+    {
+        Task<object> Insert(TEntity entity);
+        Task<int> InsertList(IEnumerable<TEntity> entityList);
+        Task<TEntity> GetById(int id);
+        Task<IEnumerable<TEntity>> GetAll();
+        Task<IEnumerable<TEntity>> GetList(Expression<Func<TEntity, bool>> predicate);
+        Task<TEntity> GetFirstOrDefault(Expression<Func<TEntity, bool>> predicate);
+        Task<bool> Update(TEntity entity);
+        Task<bool> Delete(TEntity entity);
+        Task<long> Count(Expression<Func<TEntity, bool>> predicate);
+    }
+}
